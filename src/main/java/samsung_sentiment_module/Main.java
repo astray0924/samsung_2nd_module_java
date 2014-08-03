c02f277c9f6c43574962dd1a36fbe5a597af0f5b
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2014-08-03 19:27:35 +0900

    trivial

--- src/main/java/samsung_sentiment_module/Main.java
+++ src/main/java/samsung_sentiment_module/Main.java
@@ -3,18 +3,23 @@ package samsung_sentiment_module;
 import samsung_sentiment_module.hierarchy.HierarchyModuleRunner;
 
 public class Main {
-	private enum Mode {
+	private enum Module {
 		SENTIMENT, TARGET, HIERARCHY
 	};
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		// 옵션 파싱하여 실행 모드(모듈) 및 input 파악
-		Mode mode = Mode.HIERARCHY;
+		// 옵션 파싱하여 실행 모듈 및 input 파악
+		Module mode = Module.HIERARCHY;
 
 		// switch 문으로 실행할 모듈 선택
 		SamsungModule module = null;
+		
 		switch (mode) {
-		case HIERARCHY:
+		case SENTIMENT:	// 감성 분석 모듈 
+			break;
+		case TARGET:	// 타겟 추출 모듈 
+			break;
+		case HIERARCHY:	// 타겟 구조 추정 모듈 
 			module = new HierarchyModuleRunner();
 			module.run();
 			break;
