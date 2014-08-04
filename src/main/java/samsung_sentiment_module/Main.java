0ea23e01ab568536e9b6e9253b42664502f70ee3
Jang, Kyoungrok <kyoungrok.jang@gmail.com>
2014-08-04 20:48:57 +0900

    Main added

---
+++ src/main/java/samsung_sentiment_module/Main.java
@@ -0,0 +1,17 @@
+package samsung_sentiment_module;
+
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArgumentParser parser = ArgumentParsers
+				.newArgumentParser("Sentiment Module");
+
+		parser.addArgument("Module").metavar("Sentiment | Target | Hierarchy")
+				.type(String.class).nargs("?").help("the module to run");
+
+	}
+
+}
