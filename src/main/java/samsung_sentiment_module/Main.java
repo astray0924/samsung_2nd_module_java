8d8f9379a8f8279a590735441cf8b2c6e098ad67
Jang, Kyoungrok <kyoungrok.jang@gmail.com>
2014-08-04 13:07:33 +0900

    ArgumentParsing: 모드 인식까지 완료

--- src/main/java/samsung_sentiment_module/Main.java
+++ src/main/java/samsung_sentiment_module/Main.java
@@ -1,7 +1,5 @@
 package samsung_sentiment_module;
 
-import java.util.List;
-
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
@@ -9,64 +7,92 @@ import net.sourceforge.argparse4j.inf.Namespace;
 
 public class Main {
 	private enum Module {
-		SENTIMENT, TARGET, HIERARCHY
+		SENTI, TARGET, HIERARCHY
 	};
 
-	public static void main(String[] args) throws ClassNotFoundException {
-		// 옵션 parser 구현
+	private static ArgumentParser parser = null;
+
+	private static ArgumentParser initArgumentParser() {
 		ArgumentParser parser = ArgumentParsers.newArgumentParser(
 				"sentiment module").description("samsung sentiment module");
-		parser.addArgument("-i", "--inputDir")
+
+		parser.addArgument("-m", "--mode")
+				.metavar("senti | target | hierarchy").type(String.class)
+				.nargs("?")
+				.help("the module to run: senti | target | hierarchy");
+		parser.addArgument("-i", "--inputDirPath")
 				.metavar("<file_path>")
 				.type(String.class)
 				.nargs("?")
 				.help("the path of input directory \n(required for senti and target module)");
-		parser.addArgument("outputDir").metavar("outputDir").type(String.class)
-				.nargs("?").help("the path of output directory");
-		parser.addArgument("-number_of_classes", "--classnum")
+		parser.addArgument("-o", "--outputDirPath").metavar("outputDirPath")
+				.type(String.class).nargs("?")
+				.help("the path of output directory");
+		parser.addArgument("-classnum", "--number_of_classes")
 				.metavar("N")
 				.type(Integer.class)
 				.nargs("?")
 				.setDefault(2)
 				.help("the number of sentiment classes to be used\n(2: [+/-] OR 3: [+/-/0])\n(used in senti_module)");
-		parser.addArgument("-targetDomainFile", "--tdf")
+		parser.addArgument("-target_domain", "--targetDomainFilePath")
 				.metavar("<file_path>")
 				.type(String.class)
 				.nargs("?")
 				.help("the path of target domain file\n(used in target_module)");
-		parser.addArgument("-pmi_threshold", "--pmi_thres")
+		parser.addArgument("-pmi_thres", "--pmi_threshold")
 				.metavar("<thres_val>").type(Float.class).nargs("?")
 				.setDefault(0.0F)
 				.help("the PMI threshold\n(used in target_module)");
-		parser.addArgument("-co_occurrence_threshold", "--co_thres")
+		parser.addArgument("-co_thres", "--co_occurrence_threshold")
 				.metavar("<thres_val>").type(Float.class).nargs("?")
 				.setDefault(0.0F)
 				.help("the co-occurrence threshold\n(used in target_module)");
-		parser.addArgument("-centroidFile", "--cf").metavar("<file_path>")
-				.type(String.class).nargs("?")
+		parser.addArgument("-centroid", "--centroidFilePath")
+				.metavar("<file_path>").type(String.class).nargs("?")
 				.help("the path of centroid file\n(used in hierarchy_module)");
 
-		String example = "example:\n" + "senti_module: java -jar <jar_file> "
-				+ "target_module: java -jar <jar_file> "
-				+ "hierarchy_module: java -jar <jar_file> ";
+		String example = "example:\n"
+				+ "\tsenti_module: java -jar <jar_file> -m senti -i ./input -o ./output -classnum 3\n"
+				+ "\ttarget_module: java -jar <jar_file> \n"
+				+ "\thierarchy_module: java -jar <jar_file> \n";
 		parser.epilog(example);
 
+		return parser;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		// 옵션 parser 구현
+		parser = initArgumentParser();
+		Namespace parsedArgs = null;
+
 		try {
-			Namespace res = parser.parseArgs(args);
-			System.out.println(res);
+			parsedArgs = parser.parseArgs(args);
+			System.out.println(parsedArgs);
 		} catch (ArgumentParserException e) {
 			parser.handleError(e);
 		}
 
-		//
-		// // 실행 모드
+		// 실행할 모듈 선택
+		String modeString = null;
+		Module mode = null;
+		try {
+			modeString = parsedArgs.getString("mode");
+			mode = Module.valueOf(modeString.toUpperCase());
+		} catch (NullPointerException e) {
+			System.err.println("The running mode is not specified! (-m [senti|target|hierarchy])");
+			System.exit(1);
+		} catch (IllegalArgumentException e) {
+			System.err.println("Unidentified mode: " + modeString);
+			System.exit(1);
+		}
+
 		// Module mode = Module.HIERARCHY;
 		//
 		// // switch 문으로 실행할 모듈 선택
 		// SamsungModule module = null;
 		//
 		// switch (mode) {
-		// case SENTIMENT: // 감성 분석 모듈
+		// case SENTI: // 감성 분석 모듈
 		// module = new SentimentPipeline();
 		// module.run(args);
 		// break;
