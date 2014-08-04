3d778f8656a554f899e0cc42fd0320d066f6f79f
Jang, Kyoungrok <kyoungrok.jang@gmail.com>
2014-08-04 10:28:49 +0900

    Option Parser 구현중

--- src/main/java/samsung_sentiment_module/Main.java
+++ src/main/java/samsung_sentiment_module/Main.java
@@ -1,5 +1,7 @@
 package samsung_sentiment_module;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
 import samsung_sentiment_module.hierarchy.HierarchyModuleRunner;
 import samsung_sentiment_module.sentiment.SentimentPipeline;
 
@@ -9,22 +11,35 @@ public class Main {
 	};
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		// 옵션 파싱하여 실행 모듈 및 input 파악
+		// 옵션 parser 구현
+		ArgumentParser parser = ArgumentParsers.newArgumentParser(
+				"sentiment module").description("samsung sentiment module");
+		parser.addArgument("inputDir").metavar("<input_dir>")
+				.type(String.class).nargs("?")
+				.help("the path of input directory");
+		parser.addArgument("outputDir").metavar("<output_dir>")
+				.type(String.class).nargs("?")
+				.help("the path of output directory");
+		parser.addArgument("classnum").metavar("N")
+				.type(Integer.class).nargs("?")
+				.help("the path of output directory");
+
+		// 실행 모드
 		Module mode = Module.HIERARCHY;
 
 		// switch 문으로 실행할 모듈 선택
 		SamsungModule module = null;
-		
+
 		switch (mode) {
-		case SENTIMENT:	// 감성 분석 모듈 
+		case SENTIMENT: // 감성 분석 모듈
 			module = new SentimentPipeline();
 			module.run(args);
 			break;
-		case TARGET:	// 타겟 추출 모듈 
+		case TARGET: // 타겟 추출 모듈
 			module = new SentimentPipeline();
 			module.run(args);
 			break;
-		case HIERARCHY:	// 타겟 구조 추정 모듈 
+		case HIERARCHY: // 타겟 구조 추정 모듈
 			module = new HierarchyModuleRunner();
 			module.run(args);
 			break;
